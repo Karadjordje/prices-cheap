a7eac47c3c26c3bee04094d7909ae139e04ac412
Đorđe Šveljo <sveljod@gmail.com>
2021-03-23 14:17:25 +0100

    Updated idea categories script, so we can scrape both categories and subcategories

--- jobs/idea/categories.js
+++ jobs/idea/categories.js
@@ -6,25 +6,26 @@ const populateCategories = async () => {
     const res = await fetch('https://online.idea.rs/v2/categories');
     const results = await res.json();
 
-    for (let i = 0; i < results.length; i++) {
-        const { name, description, id } = results[i];
-
+    const insertCategory = async (
+        { name, description, id, children },
+        parentId,
+    ) => {
         const slug = slugify(name.toLowerCase());
-
-        const existing = await db.Category.findOne({
+        let existing = await db.Category.findOne({
             where: {
-                slug,
+                'references.idea': id,
             },
         });
 
         if (!existing) {
-            await db.Category.create({
+            existing = await db.Category.create({
                 name,
                 description,
                 slug,
                 references: {
                     idea: id,
                 },
+                categoryId: parentId,
             });
         } else if (existing.references.idea !== id) {
             await existing.update({
@@ -32,8 +33,20 @@ const populateCategories = async () => {
                     ...existing.references,
                     id,
                 },
+                categoryId: parentId,
             });
         }
+
+        if (children && children.length > 0) {
+            await Promise.all(
+                children.map((cat) => insertCategory(cat, existing.id)),
+            );
+        }
+    };
+
+    for (let i = 0; i < results.length; i++) {
+        const { name, description, id, children } = results[i];
+        await insertCategory({ name, description, id, children });
     }
     return results;
 };
